30ba07fc124f329805bc085e0f53b715065b0b24
Matt Steiner <msteinerweb@gmail.com>
2020-09-21 12:14:13 -0500

    add wp-cli commands to bypass manual setup of WP

--- installer/modules/run.js
+++ installer/modules/run.js
@@ -2,6 +2,12 @@
  * Installation
  */
 
+const user = 'msteinerweb';
+const repo = 'wordpressify-theme';
+const branch = 'master';
+
+const settings = require('../settings');
+
 const fs = require('fs-extra');
 const theCWD = process.cwd();
 const theCWDArray = theCWD.split('/');
@@ -13,12 +19,6 @@ const download = require('download');
 const handleError = require('./handleError.js');
 const clearConsole = require('./clearConsole.js');
 const printNextSteps = require('./printNextSteps.js');
-
-
-const user = 'msteinerweb';
-const repo = 'wordpressify-theme';
-const branch = 'master';
-
 
 
 module.exports = () => {
@@ -41,10 +41,9 @@ module.exports = () => {
 	spinner.start(`1. Creating 🎈 WordPressify files inside → ${chalk.black.bgWhite(` ${theDir} `)}`);
 	download(`https://github.com/${user}/${repo}/archive/${branch}.zip`, theCWD, {extract: true})
 
-	// .then(() => extract(`${repo}-${branch}.zip`, { dir: theCWD, }))
 		.then(() => {
 			if(process.platform === 'win32') return execa('xcopy', [`${theCWD}\\${repo}-${branch}\\*.*`, theCWD, '/E/H']);
-			return execa('find', [`${theCWD}/${repo}-${branch}`, '-exec', 'mv', '{}', theCWD, '\;']);
+			return execa('rsync', ['-avzh', `${theCWD}/${repo}-${branch}/`, `${theCWD}/`]);
 		})
 		.then(() => fs.remove(`${repo}-${branch}`))
 
@@ -64,6 +63,26 @@ module.exports = () => {
 			spinner.start('3. Installing npm packages...');
 			return execa('npm', ['install'])
 		})
+
+		// install WordPress
+		.then(() => {
+			spinner.succeed();
+			spinner.start('4. Installing WordPress...');
+			return execa('wp', ['config', 'create', '--dbname=test', `--dbuser=${settings.wordpress.dbuser}`, `--dbpass=${settings.wordpress.dbpass}`, `--dbhost=${dbhost}`, '--force', `--path=${theCWD}/build/wordpress`]);
+		})
+		.then(() => execa('wp', ['core', 'install', '--url=http://127.0.0.1:3020', '--title=Test Site', `--admin_user=${settings.wordpress.admin_user}`, `--admin_password=${settings.wordpress.admin_password}`, `--admin_email=${settings.wordpress.admin_email}`, '--skip-email', `--path=${theCWD}/build/wordpress`]))
+
+		.then(() => execa('wp', ['option', 'update', 'timezone_string', 'America/Chicago', `--path=${theCWD}/build/wordpress`]))
+		.then(() => execa('wp', ['option', 'update', 'start_of_week', '0', `--path=${theCWD}/build/wordpress`]))
+		.then(() => execa('wp', ['option', 'update', 'siteurl', 'http://127.0.0.1:3020', `--path=${theCWD}/build/wordpress`]))
+		.then(() => execa('wp', ['option', 'update', 'home', 'http://127.0.0.1:3020', `--path=${theCWD}/build/wordpress`]))
+
+		.then(() => execa('wp', ['rewrite', 'structure', '/%postname%/', `--path=${theCWD}/build/wordpress`]))
+
+		.then(() => execa('wp', ['plugin', 'uninstall', '--all', '--deactivate', `--path=${theCWD}/build/wordpress`]))
+
+		.then(() => execa('wp', ['post', 'delete', '1', '2', '3', '--force', `--path=${theCWD}/build/wordpress`]))
+
 
 		.then(() => {
 			spinner.succeed();
